7b433c4a08405226f063c35b835fefd094a9b379
python12138 <err347044@gmail.com>
2024-06-22 23:37:09 +0800

    4

--- src/AvaloniaEdit.Demo/测试文件夹/A.cs
+++ src/AvaloniaEdit.Demo/测试文件夹/A.cs
@@ -3,10 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Windows.Input;
 namespace AvaloniaEdit.Demo.测试文件夹
 {
-    public class A
+    public class A : ICommand
     {
+        public event EventHandler CanExecuteChanged;
+        public Action CommandParameter { get; set; }
+
+        public A(object commandParameter)
+        {
+            this.CommandParameter = commandParameter as Action;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            this.CommandParameter();
+        }
     }
+     
+        
+      
+    
 }
