bb1d932e28961c06db3410b64960d10205c5b79b
python12138 <err347044@gmail.com>
2024-06-22 23:11:00 +0800

    测试

---
+++ src/AvaloniaEdit.Demo/测试文件夹/B.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvaloniaEdit.Demo.测试文件夹
+{
+    internal class B
+    {
+    }
+}
